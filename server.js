d16c5eb4e6b624e19b44e701f0bee85cda4a7f6f
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-11-30 21:24:23 +0000

    add static files and index.html

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const path = require("path")
 const express = require("express")  
 const app = express()
 const port = process.env.PORT || 8080
 
 
+app.use(express.static(__dirname + "/static"))
+
+
 app.get("/", (req, res) => {
-    res.send("Hello, World!")
+    res.sendFile(path.join(__dirname, "index.html"))
 })
 
 
