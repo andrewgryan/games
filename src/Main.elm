dbe3acd7906bb40e9f16a710a96a383e7e9fadf8
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-04-25 10:56:31 +0100

    regular commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,104 @@
+{-
+   Main program, note only comments can exist before the
+   module statement
+-}
+
+
 module Main exposing (main)
 
 import Html exposing (text)
 
 
+escapeEarth myVelocity mySpeed fuelStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand =
+            if fuelStatus == "low" then
+                "Land on drone ship"
+
+            else
+                "Land on launch pad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "Stay in orbit"
+
+    else
+        whereToLand
+
+
+weekDay dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+
+hashtag dayInNumber =
+    case weekDay dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+"""
+
+
 main =
-    text "Hello!"
+    revelation
+        |> text
