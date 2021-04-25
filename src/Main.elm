9269d3d9b8be00ee95eaa20304b8c10edc642422
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-04-25 11:14:02 +0100

    Elm Hello,World!

--- src/Main.elm
+++ src/Main.elm
@@ -6,99 +6,48 @@
 
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, text)
 
 
-escapeEarth myVelocity mySpeed fuelStatus =
-    let
-        escapeVelocityInKmPerSec =
-            11.186
-
-        orbitalSpeedInKmPerSec =
-            7.67
-
-        whereToLand =
-            if fuelStatus == "low" then
-                "Land on drone ship"
-
-            else
-                "Land on launch pad"
-    in
-    if myVelocity > escapeVelocityInKmPerSec then
-        "Godspeed"
-
-    else if mySpeed == orbitalSpeedInKmPerSec then
-        "Stay in orbit"
-
-    else
-        whereToLand
+type alias Model =
+    Int
 
 
-weekDay dayInNumber =
-    case dayInNumber of
-        0 ->
-            "Sunday"
-
-        1 ->
-            "Monday"
-
-        2 ->
-            "Tuesday"
-
-        3 ->
-            "Wednesday"
-
-        4 ->
-            "Thursday"
-
-        5 ->
-            "Friday"
-
-        6 ->
-            "Saturday"
-
-        _ ->
-            "Unknown day"
+type Msg
+    = NoOp
 
 
-hashtag dayInNumber =
-    case weekDay dayInNumber of
-        "Sunday" ->
-            "#SinDay"
 
-        "Monday" ->
-            "#MondayBlues"
-
-        "Tuesday" ->
-            "#TakeMeBackTuesday"
-
-        "Wednesday" ->
-            "#HumpDay"
-
-        "Thursday" ->
-            "#ThrowbackThursday"
-
-        "Friday" ->
-            "#FlashbackFriday"
-
-        "Saturday" ->
-            "#Caturday"
-
-        _ ->
-            "#Whatever"
+-- INIT
 
 
-revelation =
-    """
-    It became very clear to me sitting out there today
-    that every decision I've made in my entire life has
-    been wrong. My life is the complete "opposite" of
-    everything I want it to be. Every instinct I have,
-    in every aspect of life, be it something to wear,
-    something to eat - it's all been wrong.
-"""
+init : Model
+init =
+    0
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    text "Hello, World!"
 
 
 main =
-    revelation
-        |> text
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
