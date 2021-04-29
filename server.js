1eeb2a222a22b71e41f45195d662115660d2d1c5
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-04-29 21:18:32 +0100

    use EJS to define websocket endpoint

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 const path = require("path")
 const express = require("express")  
-const enableWs = require("express-ws")
+const expressWs = require("express-ws")
 const app = express()
 const port = process.env.PORT || 8080
 
 // Enable websockets
-let instance = enableWs(app)
+let instance = expressWs(app)
 
+let history = []
 
 app.use(express.static(__dirname + "/static"))
 
+// View engine
+app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    // res.sendFile(path.join(__dirname, "index.html"))
+    let endpoint
+    if (process.env.NODE_ENV === "production") {
+        endpoint = "wss://sheltered-basin-70535.herokuapp.com/ws"
+    } else {
+        endpoint = "ws://localhost:8080/ws"
+    }
+    res.render(path.join(__dirname, "index.ejs"), {
+        title: "Quiz",
+        endpoint
+    })
 })
 
 
 // Web socket
 app.ws("/ws", (ws, req) => {
+    console.log("websocket connection open")
+    history.forEach(msg => {
+        ws.send(msg)
+    })
+
     ws.on("message", msg => {
+        history.push(msg)
+        console.log("websocket message ", msg)
         instance.getWss("/ws").clients.forEach((client) => {
             client.send(msg)
         })
