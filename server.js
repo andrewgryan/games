478c8dacfc00dbe5cdddd7820acb589a13686491
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-05-02 08:21:52 +0100

    support socket.io

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const path = require("path")
+const http = require("http")
+const { Server } = require("socket.io")
 const express = require("express")  
-const expressWs = require("express-ws")
 const app = express()
 const port = process.env.PORT || 8080
-
-// Enable websockets
-let instance = expressWs(app)
 
 let leaderboard = []
 
@@ -17,42 +15,33 @@ app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname, "index.html"))
-    let endpoint
+    let baseUrl
     if (process.env.NODE_ENV === "production") {
-        endpoint = "wss://sheltered-basin-70535.herokuapp.com/ws"
+        baseUrl = "https://sheltered-basin-70535.herokuapp.com"
     } else {
-        endpoint = "ws://localhost:8080/ws"
+        baseUrl = "http://localhost:8080"
     }
     res.render(path.join(__dirname, "index.ejs"), {
         title: "Quiz",
-        endpoint
+        baseUrl
     })
 })
 
 
-// Web socket
-app.ws("/ws", (ws, req) => {
-    console.log("websocket connection open")
-    ws.send(JSON.stringify(leaderboard))
+let httpServer = http.createServer(app)
 
-    ws.on("message", msg => {
-        console.log("websocket message ", msg)
+// Socket.io
+const io = new Server(httpServer)
+io.on("connection", socket => {
+    console.log("a user connected")
+    socket.on("score", msg => {
         let { payload } = JSON.parse(msg)
-        console.log("websocket message ", payload)
-
         leaderboard.push(payload)
-
-        instance.getWss("/ws").clients.forEach((client) => {
-            client.send(JSON.stringify(leaderboard))
-        })
-
-        // ws.send(msg)
-    })
-    ws.on("close", () => {
-        console.log("websocket connection closed")
+        // Emit to everyone
+        io.emit("leaderboard", leaderboard)
     })
 })
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`listening on: http://localhost:${port}`)
 })
