c6c712e16d403d476b1a422c2c12a201198c6fa4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-05-28 17:14:43 +0100

    add message protocol to move game logic into client

--- server.js
+++ server.js
@@ -23,19 +23,36 @@ let httpServer = http.createServer(app)
 // Socket.io
 const io = new Server(httpServer)
 io.on("connection", socket => {
-    console.log("A user connected")
+    console.log("connected socket id", socket.id)
+
+    // Wire up enter/leave events
+    socket.emit("enter", { type: "enter", payload: { id: socket.id} })
+    socket.on("disconnect", () => {
+        socket.emit("leave", { type: "leave", payload: { id: socket.id} })
+    })
 
     // Listen for user join
+    // TODO move this to client layer
     socket.on("join", msg => {
         console.log("join: ", msg)
     })
 
     // Listen for answer
+    // TODO move this to client layer
     socket.on("answer", msg => {
         console.log("answer: ", msg)
     })
 
+    // Public & private message(s)
+    socket.on("private", msg => {
+        io.to(msg.to).emit("private", msg)
+    })
+    socket.on("public", msg => {
+        socket.broadcast.emit("public", msg)
+    })
+
     // Listen for score
+    // TODO move this to client layer
     socket.on("score", payload => {
         leaderboard.push(payload)
         // Emit to everyone
